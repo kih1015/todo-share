3b4ab7e1897dc510cc2fe22352fc0df69e7cc901
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 18:51:48 +0900

    feat: LikeService 추가

---
+++ src/main/java/kr/kro/todoshare/service/LikeService.java
@@ -0,0 +1,33 @@
+package kr.kro.todoshare.service;
+
+import jakarta.transaction.Transactional;
+import kr.kro.todoshare.controller.dto.request.LikeCreateRequest;
+import kr.kro.todoshare.domain.Like;
+import kr.kro.todoshare.repository.LikeRepository;
+import kr.kro.todoshare.repository.TaskRepository;
+import kr.kro.todoshare.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class LikeService {
+    
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
+
+    @Transactional
+    public void create(LikeCreateRequest likeCreateRequest) {
+        Like like = Like.builder()
+                .user(userRepository.findById(likeCreateRequest.user()))
+                .task(taskRepository.findById(likeCreateRequest.task()))
+                .build();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        likeRepository.deleteById(id);
+    }
+}
