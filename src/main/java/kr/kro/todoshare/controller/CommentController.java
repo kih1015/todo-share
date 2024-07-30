ca194ae7ecc319d3b2ecc2113bba438f918dd60f
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 20:10:38 +0900

    fix: CommentController 오류 수정

--- src/main/java/kr/kro/todoshare/controller/CommentController.java
+++ src/main/java/kr/kro/todoshare/controller/CommentController.java
@@ -1,9 +1,9 @@
 package kr.kro.todoshare.controller;
 
-import kr.kro.todoshare.controller.dto.request.UserCreateRequest;
-import kr.kro.todoshare.controller.dto.request.UserUpdateRequest;
-import kr.kro.todoshare.controller.dto.response.UserResponse;
-import kr.kro.todoshare.service.UserService;
+import kr.kro.todoshare.controller.dto.request.CommentCreateRequest;
+import kr.kro.todoshare.controller.dto.request.CommentUpdateRequest;
+import kr.kro.todoshare.controller.dto.response.CommentResponse;
+import kr.kro.todoshare.service.CommentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +11,33 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/comments")
 @AllArgsConstructor
 public class CommentController {
 
-    private final UserService userService;
+    private final CommentService commentService;
 
     @PostMapping()
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateRequest request) {
-        UserResponse response = userService.create(request);
-        return ResponseEntity.created(URI.create("/users/" + response.id())).body(response);
+    public ResponseEntity<CommentResponse> createComment(@RequestBody CommentCreateRequest request) {
+        CommentResponse response = commentService.create(request);
+        return ResponseEntity.created(URI.create("/comments/" + response.id())).body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUsers(@PathVariable Long id) {
-        UserResponse response = userService.getById(id);
+    public ResponseEntity<CommentResponse> getComments(@PathVariable Long id) {
+        CommentResponse response = commentService.getById(id);
         return ResponseEntity.ok().body(response);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserUpdateRequest request) {
-        UserResponse response = userService.update(id, request);
+    public ResponseEntity<CommentResponse> updateComment(@PathVariable Long id, @RequestBody CommentUpdateRequest request) {
+        CommentResponse response = commentService.update(id, request);
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<UserResponse> deleteUser(@PathVariable Long id) {
-        userService.delete(id);
+    public ResponseEntity<CommentResponse> deleteComment(@PathVariable Long id) {
+        commentService.delete(id);
         return ResponseEntity.noContent().build();
     }
 }
