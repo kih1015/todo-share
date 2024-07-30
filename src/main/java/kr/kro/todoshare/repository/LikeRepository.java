c4d3348db77f91c591b02d23f493d20c7b3e9f06
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 10:12:20 +0900

    feat: LikeRepository 생성

---
+++ src/main/java/kr/kro/todoshare/repository/LikeRepository.java
@@ -0,0 +1,18 @@
+package kr.kro.todoshare.repository;
+
+import kr.kro.todoshare.domain.Like;
+import kr.kro.todoshare.domain.Task;
+import kr.kro.todoshare.domain.User;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeRepository extends Repository<Like, Long> {
+
+    List<Like> findAllByTask(Task task);
+
+    Like save(Like like);
+
+    void delete(Like like);
+}
