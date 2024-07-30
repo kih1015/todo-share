899d6767610144c4f3cd98b3c359a24d87031e89
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 10:11:58 +0900

    feat: TaskRepository 생성

---
+++ src/main/java/kr/kro/todoshare/repository/TaskRepository.java
@@ -0,0 +1,19 @@
+package kr.kro.todoshare.repository;
+
+import kr.kro.todoshare.domain.Task;
+import kr.kro.todoshare.domain.User;
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends Repository<Task, Long> {
+
+    List<Task> findAll();
+
+    Optional<Task> findById(Long id);
+
+    Task save(Task task);
+
+    void delete(Task task);
+}
