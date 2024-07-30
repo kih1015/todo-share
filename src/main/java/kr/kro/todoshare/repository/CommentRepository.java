a5c8859bfa0a93a8b59726ba0a190e01fee77106
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 10:12:11 +0900

    feat: CommentRepository 생성

---
+++ src/main/java/kr/kro/todoshare/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package kr.kro.todoshare.repository;
+
+import kr.kro.todoshare.domain.Comment;
+import kr.kro.todoshare.domain.User;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+
+public interface CommentRepository extends Repository<Comment, Long> {
+
+    Optional<Comment> findById(Long id);
+
+    Comment save(Comment comment);
+
+    void delete(Comment comment);
+}
