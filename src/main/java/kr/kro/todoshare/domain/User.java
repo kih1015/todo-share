ee8dd88ad67c82fb40bc543784f1ac18eb7d2506
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 09:53:36 +0900

    feat: User 도메인 생성

---
+++ src/main/java/kr/kro/todoshare/domain/User.java
@@ -0,0 +1,47 @@
+package kr.kro.todoshare.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login_id")
+    private String loginId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "create_date")
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+    @OneToMany(mappedBy = "task", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+
+    public void update(String nickname, String password) {
+        this.nickname = nickname;
+        this.password = password;
+    }
+}
