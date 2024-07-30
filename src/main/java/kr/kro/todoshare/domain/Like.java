dff963f071e5ef08855a63e44bc8089d87fd8e30
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 09:54:00 +0900

    feat: Like 도메인 생성

---
+++ src/main/java/kr/kro/todoshare/domain/Like.java
@@ -0,0 +1,28 @@
+package kr.kro.todoshare.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "like")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "task")
+    private Task task;
+}
