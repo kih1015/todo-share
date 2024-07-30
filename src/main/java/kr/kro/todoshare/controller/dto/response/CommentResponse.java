301fb9bad7d8ce487f8cfcd0db72f5da1be6a562
강인화 <rkddlsghk1998@gmail.com>
2024-07-30 18:39:27 +0900

    fix: 오류 수정

--- src/main/java/kr/kro/todoshare/controller/dto/response/CommentResponse.java
+++ src/main/java/kr/kro/todoshare/controller/dto/response/CommentResponse.java
@@ -8,6 +8,6 @@ public record CommentResponse(Long id, String content, LocalDateTime createdDate
                               Long writer, Long task) {
 
     public static CommentResponse from(Comment comment) {
-        return new CommentResponse(comment.getId(), comment.getContent(), comment.getCreatedDate(), comment.getModifiedDate(), comment.getUser().getId(), comment.getTask().getId());
+        return new CommentResponse(comment.getId(), comment.getContent(), comment.getCreatedDate(), comment.getModifiedDate(), comment.getWriter().getId(), comment.getTask().getId());
     }
 }
