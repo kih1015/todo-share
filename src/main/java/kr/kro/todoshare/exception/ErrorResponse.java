101b725eb72f05f5cddd1327a2489057625580ff
강인화 <rkddlsghk1998@gmail.com>
2024-08-06 02:42:52 +0900

    feat: 불필요한 예외 삭제

--- src/main/java/kr/kro/todoshare/exception/ErrorResponse.java
+++
@@ -1,7 +0,0 @@
-package kr.kro.todoshare.exception;
-
-public record ErrorResponse(
-        String status,
-        String message
-) {
-}
--- src/main/java/kr/kro/todoshare/exception/error/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package kr.kro.todoshare.exception.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-
-    HttpStatus getHttpStatus();
-    String getMessage();
-}
