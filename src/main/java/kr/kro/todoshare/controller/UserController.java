aa2044d17247215642decb57ee486bee52d4a953
강인화 <rkddlsghk1998@gmail.com>
2024-08-06 03:14:37 +0900

    feat: 사용자 API 인증, 인가 검사 기능 추가

--- src/main/java/kr/kro/todoshare/controller/UserController.java
+++ src/main/java/kr/kro/todoshare/controller/UserController.java
@@ -1,11 +1,14 @@
 package kr.kro.todoshare.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import kr.kro.todoshare.controller.dto.request.UserLoginRequest;
 import kr.kro.todoshare.controller.dto.request.UserCreateRequest;
 import kr.kro.todoshare.controller.dto.request.UserUpdateRequest;
 import kr.kro.todoshare.controller.dto.response.UserResponse;
+import kr.kro.todoshare.exception.AccessDeniedException;
+import kr.kro.todoshare.exception.AuthenticationException;
 import kr.kro.todoshare.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,33 +24,67 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping()
-    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserResponse> createUser(
+            @Valid @RequestBody UserCreateRequest request
+    ) {
         UserResponse response = userService.create(request);
         return ResponseEntity.created(URI.create("/users/" + response.id())).body(response);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponse> getUsers(@Valid @PathVariable Long id) {
+    public ResponseEntity<UserResponse> getUser(
+            @PathVariable Long id
+    ) {
         UserResponse response = userService.getById(id);
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping("/login")
-    public String login(@Valid @RequestBody UserLoginRequest request, HttpSession session) {
-        session.setAttribute("userId", userService.login(request));
-        session.setMaxInactiveInterval(1800);
-        return "success";
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest request) {
-        UserResponse response = userService.update(id, request);
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> getMe(
+            @Parameter(hidden = true) @SessionAttribute(name = "userId", required = false) Long userId
+    ) {
+        if (userId == null) {
+            throw new AuthenticationException();
+        }
+        UserResponse response = userService.getById(userId);
         return ResponseEntity.ok().body(response);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<UserResponse> deleteUser(@PathVariable Long id) {
-        userService.delete(id);
+    @PostMapping("/login")
+    public String login(
+            @Valid @RequestBody UserLoginRequest request,
+            HttpSession session
+    ) {
+        session.setAttribute("userId", userService.login(request));
+        session.setMaxInactiveInterval(1800);
+        return "로그인 성공!";
+    }
+
+    @PostMapping("/logout")
+    public String logout(
+            HttpSession session
+    ) {
+        session.invalidate();
+        return "로그아웃 성공!";
+    }
+
+    @PutMapping()
+    public ResponseEntity<UserResponse> updateUser(
+            @Parameter(hidden = true) @SessionAttribute(name = "userId", required = false) Long userId,
+            @Valid @RequestBody UserUpdateRequest request
+    ) {
+        if (userId == null) {
+            throw new AuthenticationException();
+        }
+        UserResponse response = userService.update(userId, request);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<UserResponse> deleteUser(
+            @Parameter(hidden = true) @SessionAttribute(name = "userId", required = false) Long userId
+    ) {
+        userService.delete(userId);
         return ResponseEntity.noContent().build();
     }
 }
