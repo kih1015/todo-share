ed03e745c8edfbc7d37688ab329f235d5326d4f5
강인화 <rkddlsghk1998@gmail.com>
2024-08-10 14:22:15 +0900

    refactor: 좋아요 중복 체크 로직 수정

--- src/main/java/kr/kro/todoshare/service/LikeService.java
+++ src/main/java/kr/kro/todoshare/service/LikeService.java
@@ -3,12 +3,11 @@ package kr.kro.todoshare.service;
 import kr.kro.todoshare.controller.dto.request.LikeCreateRequest;
 import kr.kro.todoshare.controller.dto.response.LikeResponse;
 import kr.kro.todoshare.domain.Like;
-import kr.kro.todoshare.domain.Task;
+import kr.kro.todoshare.domain.User;
 import kr.kro.todoshare.exception.ConflictException;
 import kr.kro.todoshare.exception.ResourceNotFoundException;
 import kr.kro.todoshare.mapper.LikeMapper;
 import kr.kro.todoshare.repository.LikeRepository;
-import kr.kro.todoshare.repository.TaskRepository;
 import kr.kro.todoshare.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,15 +19,12 @@ public class LikeService {
     
     private final LikeRepository likeRepository;
     private final UserRepository userRepository;
-    private final TaskRepository taskRepository;
     private final LikeMapper likeMapper;
 
     @Transactional
     public LikeResponse create(Long userId, LikeCreateRequest request) {
-        Task task = taskRepository.findById(request.task()).orElseThrow(ResourceNotFoundException::new);
-        if (likeRepository.findAllByTask(task)
-                .stream()
-                .anyMatch(like -> like.getUser().getId().equals(userId))) {
+        User user = userRepository.findById(userId).orElseThrow(ResourceNotFoundException::new);
+        if (user.getLikes().stream().anyMatch(like -> like.getTask().getId().equals(request.task()))) {
             throw new ConflictException("이미 좋아요를 눌렀습니다.");
         }
         Like like = likeMapper.toEntity(userId, request);
